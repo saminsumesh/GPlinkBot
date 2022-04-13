f209b665b2baeb7c88c4681044a67a5118894902
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-14 02:17:19 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+from os import environ
+
+# Get it from your Telegram Developer site
+
+API_ID = int(environ.get("API_ID"))
+
+# Get it from your Telegram Developer site
+
+API_HASH = environ.get("API_HASH")
+
+# Your bot token from @botfather
+
+TG_BOT_TOKEN = environ.get("TG_BOT_TOKEN")
+
+# GPlink API Key
+
+API_KEY = environ.get("API_KEY")
