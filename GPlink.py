8cb6c3b59f9491ce19788d7603fe70b6337d1af8
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-14 10:36:46 +0530

    Update GPlink.py

--- GPlink.py
+++ GPlink.py
@@ -5,7 +5,7 @@ from bot import GPLink
 
 client = GPLink()
 
-@client.on_message(filters.regex(r"https:?//[^\s]=+") & filters.private))
+@client.on_message(filters.regex(r"https:?//[^\s]=+") & filters.private)
 async def url_handler(client, message):
     link = message.matches[0].group(0)
     try:
