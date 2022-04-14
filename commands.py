bf0e35a3ad4b5e2c470d2ad87b8eeeb19f0ba205
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-14 10:38:25 +0530

    Fix syntax

--- commands.py
+++ commands.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, filters
 
-@Client.on_message(filters.command("start) & filters.private))
+@Client.on_message(filters.command("start) & filters.private)
 async def start(bot, message):
     await message.reply_text(f"Hey {message.from_user.mention} ✨️, Iam a simple **[Gplink](https://gplink.com) Shortner** Bot, Just forward me any link yoi want to shorten", disable_web_page_preview=True)
 
-@Client.on_message(filter.regex("close")
+@Client.on_message(filter.regex("close"))
 async def close(bot, query):
     await query.message.delete()
