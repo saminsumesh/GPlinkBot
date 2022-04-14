bc85a9688088160a20f073779da1deb45932b5c4
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-14 11:29:06 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("start) & filters.private)
 async def start(bot, message):
-    await message.reply_text(f"Hey {message.from_user.mention} ✨️, Iam a simple **[Gplink](https://gplink.com) Shortner** Bot, Just forward me any link yoi want to shorten", disable_web_page_preview=True)
+    await message.reply_text(f"Hey {message.from_user.mention} ✨️, Iam a simple **[Gplink](https://gplinks.in) Shortner** Bot, Just forward me any link yoi want to shorten", disable_web_page_preview=True)
 
 @Client.on_message(filter.regex("close"))
 async def close(bot, query):
