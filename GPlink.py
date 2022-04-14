38ff4502546220d7bd582d5131dc87122fb16478
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-14 12:10:06 +0530

    Update GPlink.py

--- GPlink.py
+++ GPlink.py
@@ -21,15 +21,17 @@ async def close(bot, query):
     await query.message.delete()
 
 
-@client.on_message(filters.regex(r"https:?//[^\s]=+") & filters.private)
+@client.on_message(filters.regex(r"https:?//[^\s]+") & filters.private)
 async def url_handler(client, message):
     link = message.matches[0].group(0)
     try:
         short_url = await make_shorturl(link)
         await message.reply_text(
             text="Here is your shortened link",
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Short Link", url=f"{short_url}"),
-                                                InlineKeyboardButton("Close", callback_data="close")]]),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Short Link", url=f"{short_url}")
+                                              ],[
+                                                InlineKeyboardButton("Close", callback_data="close")
+                                              ]]),
             disable_web_page_preview=True)
     except Exception as e:
         await message.reply_text(f"There was an error {e}", qoute=True)
